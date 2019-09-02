6b111c312801456b0be35ef485f096178efcf340
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-02 08:32:38 -0700

    Update contents of insert example

--- insert.py
+++ insert.py
@@ -1,26 +1,21 @@
-# Create an empty list
-the_list = []
-
-# Can only use [] to update elements that already exist
-# the_list[0] = 42
-
-# Can insert into an empty list using:
 # insert(index, value)
-the_list.insert(0, 10)
-print(the_list)
 
-# insert() with index = 0 always inserts at the beginning of the list
-the_list.insert(0, 20)
-print(the_list)
+# insert() with index = 0 always prepends to the list
+the_list = [0, 1, 2]
+the_list.insert(0, 42)
 
 # insert() with index >= len(list) always appends to the list
-the_list.insert(999, 30)
-print(the_list)
+the_list = [0, 1, 2]
+the_list.insert(len(the_list), 42)
+the_list.insert(999, 1000)
 
 # In general, insert() inserts *before* the given index
-the_list.insert(1, 40)
-print(the_list)
+the_list = [0, 1, 2]
+the_list.insert(1, 42)
 
-# Update an existing element using []
-the_list[0] = 50
-print(the_list)
+# It is safe to insert into an empty list
+the_list = []
+the_list.insert(0, 42)
+
+the_list = []
+the_list.insert(999, 42)
