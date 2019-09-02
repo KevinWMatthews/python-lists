54c70627930e10540391187f65d6fc9643a04f61
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-02 09:58:34 -0700

    Split indexing script
    Separate indexing with an integer key
    and indexing with a slice key.

--- index_slice.py
+++ index_slice.py
@@ -1,18 +1,6 @@
 # [] is Python's index operator
 # The key can be an integer or a slice!
 
-# Retrieve a single item
-the_list = [0, 1, 2]
-result = the_list[1]
-
-# Update a single item
-the_list = [0, 1, 2]
-the_list[1] = 42
-
-# [] with an integer key can only update elements that already exist
-the_list = []
-# the_list[0] = 42  # Raises an IndexError
-
 # Retrieve mutliple items using a slice
 the_list = [0, 1, 2, 3]
 result = the_list[1:3]
